9b63b18da952ce9d29a825010554d39852d6c774
Xiao <vincent@cwi.nl>
2021-03-27 13:11:48 +0100

    update

--- src/compute_ANI.py
+++ src/compute_ANI.py
@@ -9,12 +9,10 @@ def compute_ANI_ij(fa,i,j,outdir):
     try:
         i_fa="{}/seq.{}.fa".format(outdir,i)
         j_fa="{}/seq.{}.fa".format(outdir,j)
-        print(i_fa)
         os.system("head -{} {}|tail -2 >{}".format(i*2,fa,i_fa))
 
         os.system("head -{} {}|tail -2 >{}".format(j*2,fa,j_fa))
         os.system("fastANI -q {} -r {} -o {}/ani.{}.{}.out -t 1 >/dev/null 2>&1".format(i_fa,j_fa,outdir,i,j))
-        # os.system("fastANI -q {} -r {} -o ani.{}.{}.out -t 1 >/dev/null 2>&1".format(i_fa,j_fa,i,j))
         os.system("rm -f {} {}".format(i_fa,j_fa))
     except:
         raise Exception("Failed to extract sequences for ANI computation.")
